b92ff64bc84d99f15af17d4adc7efba0a57faaa1
Hassan Azhar <hassanazhar173732@gmail.com>
2023-12-04 21:41:01 +0100

    refactored server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 // Middleware for logging incoming requests
-app.use(function(req,res,next){
+app.use(function(req, res, next){
   console.log(req.method + ' ' + req.path + ' - ' + req.ip);
   next();
 });
@@ -13,10 +13,10 @@ if (!process.env.DISABLE_XORIGIN) {
   app.use((req, res, next) => {
     const allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
     const origin = req.headers.origin || '*';
-    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
-         console.log(origin);
-         res.setHeader('Access-Control-Allow-Origin', origin);
-         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if (!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1) {
+      console.log(origin);
+      res.setHeader('Access-Control-Allow-Origin', origin);
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     }
 
     next();
@@ -25,28 +25,25 @@ if (!process.env.DISABLE_XORIGIN) {
   app.use('/public', express.static(__dirname + '/public'));
 
   app.get("/", (req, res) => {
-    res.sendFile( __dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
   });
 
-  app.get("/now",(req,res,next) => {
+  app.get("/now", (req, res, next) => {
     currentTime = new Date();
     req.time = currentTime.toString();
     next();
-  }, (req,res) => {
-    res.send({"time" : req.time});
+  }, (req, res) => {
+    res.send({"time": req.time});
   });
 
-  app.get("/json" , (req, res) => {
-    var response = "Hello json"
-    if(process.env.MESSAGE_STYLE === "uppercase"){
-      res.json({"message": response.toUpperCase()})
-    }
-    else{
-      res.json({"message": response})
+  app.get("/json", (req, res) => {
+    var response = "Hello json";
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+      res.json({"message": response.toUpperCase()});
+    } else {
+      res.json({"message": response});
     }
   });
-
-  
 }
 
 const port = process.env.PORT || 3000;
